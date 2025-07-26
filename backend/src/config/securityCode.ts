4bf9756ea80150cda0854b07f35dc6aa7a4894d3
FlorenciaPons <ponsmariaflorencia@gmail.com>
2025-07-26 18:34:27 -0300

    Update

--- backend/src/config/securityCode.ts
+++
@@ -1,8 +0,0 @@
-export const generateCode = () => {
-    const length = 6;
-    const otp = Math.floor(
-      Math.pow(10, length - 1) +
-        Math.random() * (Math.pow(10, length) - Math.pow(10, length - 1))
-    );
-    return otp.toString();
-  };
